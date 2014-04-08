d4c1ab8cc5660928e309b6e1f69d327babd25d90
BucktownCLo <BucktownClo@live.com>
2014-04-08 11:38:34 -0500

    updated

--- PoolCalculator/src/HotTubs.java
+++ PoolCalculator/src/HotTubs.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.Event.*;
+import java.awt.event.WindowAdapter;
+
 import javax.swing.*;
 
 
@@ -14,20 +16,48 @@ public class HotTubs extends JPanel
 		JFrame frame = new JFrame("Lister v1.0");
 		
 		JPanel hotTubPanel = new JPanel();
+		
 		final ButtonGroup hotTubGroup = new ButtonGroup();
+		
 		JRadioButton radioButton;
+		
 		hotTubPanel.add(radioButton = new JRadioButton("Hot Tub"));
 		radioButton.setActionCommand("Hot Tub");
 		hotTubGroup.add(radioButton);
+		
 		hotTubPanel.add(radioButton = new JRadioButton("Oval Tub"));
 		radioButton.setActionCommand("Oval Tub");
 		hotTubGroup.add(radioButton);
 		
 		
-		Container content = frame.getContentPane();
-		content.setLayout(new GridLayout(3, 1));
-		content.add(hotTubPanel);
+		cmdCalculate = new JButton("Calculate");
+		cmdExit		 = new JButton("Exit");
 		
+		JFrame.addWindowListener(new WindowAdapter)
+		
+		
+		
+//		mainFrame = new JFrame("CIS355A Week5 Example Project");
+//        cmdGood = new JButton("Good");
+//         cmdBad = new JButton("Bad");
+//         
+//         Container c = mainFrame.getContentPane();
+//         c.setLayout(new FlowLayout());
+//         
+//         c.add(cmdGood);
+//         c.add(cmdBad);
+//        
+//        cmdGood.setMnemonic('G');
+//        cmdBad.setMnemonic('B');
+//		mainFrame.setSize(300, 200);
+//        
+//        mainFrame.addWindowListener(new WindowAdapter()
+		
+		
+//		Container content = frame.getContentPane();
+//		content.setLayout(new GridLayout(3, 1));
+//		content.add(hotTubPanel);
+//		
 		
 		
 		
