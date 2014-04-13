22fd4bc62c31b0bc09871e159db8a6a66a215b10
fox <fox@192.168.1.7>
2014-04-13 15:41:11 -0500

    Added GENERAL Page + General page COMPLETE

--- PoolCalculator/src/GroupProject.java
+++ PoolCalculator/src/GroupProject.java
@@ -5,53 +5,49 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-
-public class GroupProject extends JFrame
-{
+public class GroupProject extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
-	public GroupProject() 
-	{
+
+	public GroupProject() {
 		setTitle("Group Project");
 		setLayout(new BorderLayout());
 		JTabbedPane gui2 = new JTabbedPane();
 		getContentPane().add(gui2);
-		
+
+		gui2.addTab("General", new General());
 		gui2.addTab("Swimming Pool", new PoolCalculator());
 		gui2.addTab("Hot Tubs", new HotTubs());
 		gui2.addTab("Conversion", new Conversion());
 		gui2.addTab("Settings", new Settings());
 		gui2.addTab("Temperature Conversion", new TemperatureCalc());
-		
-		//gui2.addTab();
-		//gui2.addTab();
-		//gui2.addTab();
-		
-		
+
+		// gui2.addTab();
+		// gui2.addTab();
+		// gui2.addTab();
+
 		getContentPane().add(gui2);
 		setVisible(true);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int screenHeight = screenSize.height;
 		int screenWidth = screenSize.width;
-		    
+
 		setSize(screenWidth / 2, screenHeight / 2);
 		setLocation(screenWidth / 4, screenHeight / 4);
-		
+
 	}
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		GroupProject tab2 = new GroupProject();
+		new General();
 		new PoolCalculator();
 		new HotTubs();
 		new Conversion();
 		new Settings();
 		new TemperatureCalc();
-		
-		
+
 		tab2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
 
 }
