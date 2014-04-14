611c5999231316e4a040e9d438c8ba2213bb65ea
Rjacks12 <regis.jackson@gmail.com>
2014-04-14 17:47:11 -0500

    updated layout

--- PoolCalculator/src/PoolCalculator.java
+++ PoolCalculator/src/PoolCalculator.java
@@ -1,4 +1,4 @@
-import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -17,7 +17,7 @@ import javax.swing.JTextField;
 
 public class PoolCalculator extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
 
 	private JLabel poolWidthLabel;
