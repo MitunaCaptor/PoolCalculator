1608059321ca6cb0f2f26cd9da2197b7526123e6
MitunaCaptor <bgarbie@my.devry.edu>
2014-04-13 19:44:46 -0500

    fixed radio buttons

--- PoolCalculator/src/HotTubs.java
+++ PoolCalculator/src/HotTubs.java
@@ -1,82 +1,69 @@
-
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-
-public class HotTubs extends JPanel
-{
+public class HotTubs extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JButton cmdCalculate;
 	private JButton cmdExit;
 	private JFrame mainFrame;
 
-	public HotTubs() 
-	{
-		
+	public HotTubs() {
+
 		mainFrame = new JFrame();
-		
-		JPanel hotTubPanel = new JPanel();
-		
-		final ButtonGroup hotTubGroup = new ButtonGroup();
-		
+
+		ButtonGroup hotTubGroup = new ButtonGroup();
+
 		JRadioButton radioButton;
-		
-		hotTubPanel.add(radioButton = new JRadioButton("Hot Tub"));
+
+		add(radioButton = new JRadioButton("Hot Tub"));
 		radioButton.setActionCommand("Hot Tub");
 		hotTubGroup.add(radioButton);
-		
-		hotTubPanel.add(radioButton = new JRadioButton("Oval Tub"));
+
+		add(radioButton = new JRadioButton("Oval Tub"));
 		radioButton.setActionCommand("Oval Tub");
 		hotTubGroup.add(radioButton);
-		
-		
-		
+
 		cmdCalculate = new JButton("Calculate");
-		cmdExit		 = new JButton("Exit");
+		cmdExit = new JButton("Exit");
 		mainFrame = new JFrame();
-		
+
 		add(cmdCalculate);
 		add(cmdExit);
-		
-		
-		mainFrame.addWindowListener(new WindowAdapter()
-		 {
-        	public void windowClosing(WindowEvent e)
-        	{
-        		System.exit(0);
-        	}
-          });
-		
-		
-		
-//		mainFrame = new JFrame("CIS355A Week5 Example Project");
-//        cmdGood = new JButton("Good");
-//         cmdBad = new JButton("Bad");
-//         
-//         Container c = mainFrame.getContentPane();
-//         c.setLayout(new FlowLayout());
-//         
-//         c.add(cmdGood);
-//         c.add(cmdBad);
-//        
-//        cmdGood.setMnemonic('G');
-//        cmdBad.setMnemonic('B');
-//		mainFrame.setSize(300, 200);
-//        
-//        mainFrame.addWindowListener(new WindowAdapter()
-		
-		
-//		Container content = frame.getContentPane();
-//		content.setLayout(new GridLayout(3, 1));
-//		content.add(hotTubPanel);
-//		
-		
-		
-		
+
+		mainFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
+		// mainFrame = new JFrame("CIS355A Week5 Example Project");
+		// cmdGood = new JButton("Good");
+		// cmdBad = new JButton("Bad");
+		//
+		// Container c = mainFrame.getContentPane();
+		// c.setLayout(new FlowLayout());
+		//
+		// c.add(cmdGood);
+		// c.add(cmdBad);
+		//
+		// cmdGood.setMnemonic('G');
+		// cmdBad.setMnemonic('B');
+		// mainFrame.setSize(300, 200);
+		//
+		// mainFrame.addWindowListener(new WindowAdapter()
+
+		// Container content = frame.getContentPane();
+		// content.setLayout(new GridLayout(3, 1));
+		// content.add(hotTubPanel);
+		//
 
 	}
 
