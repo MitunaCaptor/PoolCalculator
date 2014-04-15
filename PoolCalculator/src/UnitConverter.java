29c2c48e66ddb94a94c9eb22aecadf18951d9c7b
Mituna <devry.edu>
2014-04-14 23:51:43 -0500

    code fix for nathan

--- PoolCalculator/src/UnitConverter.java
+++ PoolCalculator/src/UnitConverter.java
@@ -1,21 +1,15 @@
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class UnitConverter extends JFrame {
+public class UnitConverter extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final int WIDTH = 300;
-	private static final int HEIGHT = 300;
-
-	private JFrame frame;
-	private JPanel content;
 
 	private JLabel lblCentimeter, lblInch, lblMeter, lblFeet;
 	private JTextField txtCentimeter, txtInch, txtMeter, txtFeet;
@@ -24,9 +18,6 @@ public class UnitConverter extends JFrame {
 
 	public UnitConverter() // constructor
 	{
-		frame = new JFrame("Unit Converter");
-		content = new JPanel();
-		frame.add(content);
 
 		lblCentimeter = new JLabel("Centimeter");
 		lblInch = new JLabel("Inch");
@@ -43,24 +34,20 @@ public class UnitConverter extends JFrame {
 		txtResult = new JTextField(10);
 		txtResult.setEditable(false);
 
-		content.setLayout(new GridLayout(5, 3));
+		setLayout(new GridLayout(5, 3));
 
-		content.add(lblCentimeter);
-		content.add(txtCentimeter);
-		content.add(lblInch);
-		content.add(txtInch);
+		add(lblCentimeter);
+		add(txtCentimeter);
+		add(lblInch);
+		add(txtInch);
 
-		content.add(lblMeter);
-		content.add(txtMeter);
-		content.add(lblFeet);
-		content.add(txtFeet);
+		add(lblMeter);
+		add(txtMeter);
+		add(lblFeet);
+		add(txtFeet);
 
-		content.add(btnConvert);
-		content.add(txtResult);
-
-		frame.setSize(WIDTH, HEIGHT);
-		frame.setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(btnConvert);
+		add(txtResult);
 
 	}
 }
